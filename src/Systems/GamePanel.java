c0c5b5cc2bbd621e2baed0b2f76c180b49f0994e
Aidy199258 <45352260+Aidy199258@users.noreply.github.com>
2020-08-26 00:01:10 +1200

    Add files via upload

---
+++ src/Systems/GamePanel.java
@@ -0,0 +1,14 @@
+package Systems;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public static int width;
+    public static int height;
+    public GamePanel(int width, int height){
+        setPreferredSize(new Dimension(width, height));
+        setFocusable(true);
+        requestFocus();
+    }
+}
