638d5ea0a7c8d7d8e14925de677f935b232eaa17
Aidy199258 <45352260+Aidy199258@users.noreply.github.com>
2020-08-25 23:14:58 +1200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import Entities.Puppy;
 import Systems.GameControl;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         //This is a 2D java game called The Flying Puppy written with ECS framework
@@ -9,18 +11,22 @@ public class Main {
         Puppy puppy = new Puppy();
         GameControl.StartGame(puppy);
 
+        JWindow jwindow = new JWindow();
+        //jwindow.
         boolean RestartGame = false;
         //......Codes for choices to restart game
 
+        Boolean ExitGame = false;
         while(true) {
-            //If puppy has no life left, Game Over.
-            if (puppy.getLife()) {
+            if (ExitGame){ break;}//Break the loop if Exit Button is pressed
+
+            //Codes running program
+            
+            if (!puppy.getLife()) {//Puppy died - Game over
                 System.out.println("Game Over :( ");
-                if (RestartGame) {
-                    GameControl.StartGame(puppy);
-                    RestartGame = false;
-                }
-            }else if (RestartGame){
+
+            }
+            if (RestartGame){
                 //If player clicks Restart Game button any time
                 GameControl.StartGame(puppy);
                 RestartGame = false;
